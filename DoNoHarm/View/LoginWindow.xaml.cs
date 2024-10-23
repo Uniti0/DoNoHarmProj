3d28861a672e864a1f9678de798f36e3dce830da
Igor <igor.sannikoff2016@yandex.ru>
2024-10-23 11:13:32 +0300

    Initial Commit

---
+++ DoNoHarm/View/LoginWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using DoNoHarm.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DoNoHarm.View
+{
+    /// <summary>
+    /// Логика взаимодействия для LoginWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            DataContext = new LoginViewModel(this);
+            InitializeComponent();
+        }
+    }
+}
---
+++ DoNoHarm/ViewModel/LoginViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Prism.Mvvm;
+using Prism.Commands;
+using System.Security.Permissions;
+using System.Windows;
+
+namespace DoNoHarm.ViewModel
+{
+    internal class LoginViewModel : BindableBase
+    {
+        private Window window;
+        private string _login;
+        private string _password;
+        private DelegateCommand _authorizate;
+
+        public string Login { get => _login; set => _login = value; }
+        public string Password { get => _password; set => _password = value; }
+
+        public DelegateCommand Authorizate { get => _authorizate; set => _authorizate = value; }
+
+        private void AuthorizateRealiztion()
+        {
+            new MainWindow().Show();
+            window.Close();
+        }
+
+        public LoginViewModel(Window window)
+        {
+            _authorizate = new DelegateCommand(AuthorizateRealiztion);
+            this.window = window;
+        }
+
+    }
+}
