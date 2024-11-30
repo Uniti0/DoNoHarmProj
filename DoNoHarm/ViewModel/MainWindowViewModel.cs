e64313ca6e542f537c44f4b54cc1b59c6a685d6d
Igor <igor>
2024-11-30 09:54:22 +0300

    Настроил навигацию и отображение услуг

--- DoNoHarm/ViewModel/MainWindowViewModel.cs
+++ DoNoHarm/ViewModel/MainWindowViewModel.cs
@@ -1,7 +1,10 @@
 ﻿using DoNoHarm.Model;
+using DoNoHarm.View.Pages;
+using Prism.Commands;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Permissions;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Controls;
@@ -11,9 +14,43 @@ namespace DoNoHarm.ViewModel
     internal class MainWindowViewModel
     {
 
+        private Page ServicePage;
+        private Page UserPage;
+
+
+        public DelegateCommand navigateService { get; set; }
+        public DelegateCommand navigateUsers { get; set; }
+
         public MainWindowViewModel()
         {
-            
+            navigateService = new DelegateCommand(NavigateService);
+            navigateUsers = new DelegateCommand(NavigateUsers);
         }
+
+
+        private void NavigateUsers()
+        {
+            if (Navigation.ActivePage != UserPage || UserPage == null)
+            {
+                UserPage = new UserPage();
+                Navigation.Navigate(UserPage);
+            } else if (Navigation.ActivePage != UserPage || UserPage != null)
+            {
+                Navigation.Navigate(UserPage);
+            }
+        }
+
+        private void NavigateService()
+        {
+            if (Navigation.ActivePage != ServicePage || ServicePage == null)
+            {
+                ServicePage = new ServicePage();
+                Navigation.Navigate(ServicePage);
+            } else if (Navigation.ActivePage != ServicePage || ServicePage != null)
+            {
+                Navigation.Navigate(ServicePage);
+            }
+        }
+
     }
 }
--- DoNoHarm/ViewModel/SerivcePageViewModel.cs
+++ DoNoHarm/ViewModel/SerivcePageViewModel.cs
@@ -1,23 +1,34 @@
 ﻿using DoNoHarm.Data;
+using Prism.Commands;
 using Prism.Mvvm;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Data.Entity;
+using System.Data.Entity.Migrations.Model;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Documents;
 
 namespace DoNoHarm.ViewModel
 {
     internal class SerivcePageViewModel : BindableBase
     {
-        public ObservableCollection<services> dgSourse { get; set; }
+        public static ObservableCollection<services> dgSourse { get; set; }
         public SerivcePageViewModel()
         {
             dgSourse = new ObservableCollection<services>();
-            DoNoHarmDB.GetContext().services.ToListAsync().Wait(); 
+            Update();
         }
 
+        private async void Update()
+        {
+            var service = await DoNoHarmDB.GetContext().services.ToListAsync();
+            foreach (services item in service)
+            {
+                dgSourse.Add(item);
+            }
+        }
     }
 }
